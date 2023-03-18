6b8710d6d6eff9838da437e9b9159b7ba47bcc57
Kei <zoschicab@gmail.com>
2023-03-18 17:36:44 +0900

    squooshからsharpに移行

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,37 @@
-import { ImagePool } from "@squoosh/lib";
-import { cpus } from "os";
+import sharp from "sharp";
 import fse from "fs-extra";
 import path from "path";
 import { Command } from "commander";
 
-const imagePool = new ImagePool(cpus().length);
-
 const program = new Command();
 program
-  .requiredOption("-i, --input <type>", "ソースディレクトリ（必須）")
-  .option("-o, --out <type>", "出力先ディレクトリ")
-  .option("-m, --minify", "画像の最適化を行う（同一拡張子での変換）", false)
+  .requiredOption("-i, --input <string>", "ソースディレクトリ（必須）")
   .option(
-    "-r, --replace-suffix",
-    "拡張子をwebpに置き換える（デフォルトはwebpを後ろに付ける）",
-    false
+    "-o, --out <string>",
+    "出力先ディレクトリ (default: ソースディレクトリ)"
   )
+  .option("-q, --quality <number>", "画質", parseInt, 75)
+  .option("-r, --replace-suffix", "拡張子をwebpに置き換える", false)
   .parse();
 
 type Options = {
   input: string;
   out?: string;
   replaceSuffix: boolean;
-  quality?: number;
+  quality: number;
 };
 const Options = program.opts<Options>();
 const IMAGE_DIR = Options.input;
 const OUTPUT_DIR = Options.out ?? IMAGE_DIR;
 const REPLACE_SUFFIX = Options.replaceSuffix;
-const QUALITY = Options.quality ?? 75;
+const QUALITY = Options.quality;
 
 if (!fse.existsSync(IMAGE_DIR)) {
   throw Error("ソースディレクトリが存在しません");
+}
+
+if (!fse.existsSync(OUTPUT_DIR)) {
+  fse.mkdirSync(OUTPUT_DIR, { recursive: true });
 }
 
 const imageFileList = fse
@@ -46,34 +46,34 @@ const imageFileList = fse
 
 const ts_start = Date.now();
 
+let successCount = 0;
+let errorCount = 0;
+
 await Promise.all(
   imageFileList.map(async (imagePath) => {
     const fileExtension = path.extname(imagePath).substring(1);
     const sourcePath = path.join(IMAGE_DIR, imagePath);
-    let destinationPath = path.join(OUTPUT_DIR, imagePath);
+    let outputPath = path.join(OUTPUT_DIR, imagePath);
 
-    const rawImageFile = await fse.readFile(sourcePath);
+    outputPath = REPLACE_SUFFIX
+      ? outputPath.slice(0, fileExtension.length * -1) + "webp"
+      : outputPath + ".webp";
 
-    const ingestedImage = imagePool.ingestImage(rawImageFile);
-
-    await ingestedImage.encode({
-      webp: {
+    await sharp(sourcePath)
+      .webp({
         quality: QUALITY,
-      },
-    });
-
-    const encodedImage = await ingestedImage.encodedWith.webp;
-
-    if (encodedImage) {
-      destinationPath = REPLACE_SUFFIX
-        ? destinationPath.slice(0, fileExtension.length * -1) + "webp"
-        : destinationPath + ".webp";
-
-      await fse.outputFile(destinationPath, encodedImage.binary);
-    }
+        lossless: false,
+      })
+      .toFile(outputPath)
+      .then(() => {
+        successCount++;
+      })
+      .catch(() => {
+        errorCount++;
+      });
   })
 );
 
 // 結果表示
 console.info("done!", "(", "total:", Date.now() - ts_start, "ms", ")");
-await imagePool.close();
+console.info("success: ", successCount, "  error: ", errorCount);
